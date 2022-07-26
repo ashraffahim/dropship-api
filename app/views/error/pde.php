450f9bc375fa7d33f298d3d5833ed4068da7ca03
Ashraf <ashraf.fahim75@gmail.com>
2022-07-26 17:20:04 +0400

    Cleaning

--- app/views/error/pde.php
+++
@@ -1,8 +0,0 @@
-<div class="container-fluid">
-	<div class="row">
-		<div class="col-12 text-center">
-			<img src="/assets/img/no-data.png" alt="No Data" height="400">
-			<div><span class="text-primary lead">Product not found!</span></div>
-		</div>
-	</div>
-</div>
