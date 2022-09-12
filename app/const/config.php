98a28f0374d1279b8b6eb7c3c287be3fd68ad5e0
Ashraf <ashraf.fahim75@gmail.com>
2022-09-13 00:01:47 +0400

    Missing File Patch

--- app/const/config.php
+++ app/const/config.php
@@ -29,4 +29,7 @@ define('TECH_EMAIL', 'support@alghaim.com');
 define('SMTP', 'mail.grap.store');
 define('SMTP_PORT', '2525');
 
+// Payment API Env Var
+define('STRIPE_API_KEY', 'sk_test_51JGXp4IoGl18YWC8sqlP7TeCjGpezoYpn45HwDmSUWGmNLCeKG5EfdY2ZUYCQATLhovA4HuevEdSPH1Xp0yGhqFI00tEdFGqMx');
+define('STRIPE_ENDPOINT_SECRET', 'whsec_66fbe7b4834241a1ec5e3faebcacb3f1b8e4e4d49f1474eef8b0c46fdc02c50f');
 ?>
